0339ae61622a87c00fefab31b7d7595fe73bab5a
Trisha Mia Morales <moralestrishamia@gmail.com>
2025-09-05 18:45:28 +0800

    added new info

--- includes/config.php
+++ includes/config.php
@@ -5,8 +5,8 @@
    
     $conn = mysqli_connect($db_host, $db_username, $db_passwd) or die("Could not connect!\n");
 
-    echo "Connection established.\n";
+    // echo "Connection established.\n";
     $db_name = "db_music";
     mysqli_select_db($conn, $db_name) or die("Could not select the database $dbname!\n". mysqli_error($conn));
-    echo "Database $db_name is selected.\n";
+    // echo "Database $db_name is selected.\n";
 ?>
